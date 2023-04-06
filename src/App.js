fdd94437c73aaad65ab0db942105647d15621748
koyel120601 <ahana120601@gmail.com>
2023-04-06 23:45:30 +0530

    first_commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+//import logo from './logo.svg';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.css'
+
+function App() {
+  return (
+    
+      
+      <form action="/action_page.php">
+        
+       
+        <br/>Date : <input type = "date"/> <br/>
+        <button type="Submit" class="btn btn-primary">Submit</button>
+ 
+       
+
+    </form>
+    
+  );
+}
+
+export default App;
