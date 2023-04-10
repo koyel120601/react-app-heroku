cb142cdf20f887d438926b965207dd3db0c199cf
koyel120601 <ahana120601@gmail.com>
2023-04-10 10:16:23 +0530

    trial3

--- src/App.js
+++ src/App.js
@@ -2,21 +2,61 @@
 import './App.css';
 //import 'bootstrap/dist/css/bootstrap.css'
 
-function App() {
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: ""
+    };
+    this.formRef = React.createRef();
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    console.log("handleSubmit called");
+    const outputDiv = document.getElementById('output');
+    const formData = new FormData(this.formRef.current);
+    fetch('http://localhost:5000/', {
+      method: 'POST',
+      body: formData
+    })
+      .then(response => response.json())
+      .then(data => {
+        console.log(data);
+        outputDiv.innerHTML = data;
+      })
+      .catch(error => {
+        console.error(error);
+        outputDiv.innerHTML = "Error: " + error.message;
+      });
+  }
+
+  render() {
+    const { data } = this.state;
   return (
-    
-      
-      <form action="/action_page.php">
+    <div className="App">
+   
+   <div id="input">
+        <form  ref={this.formRef} method='POST' onSubmit={this.handleSubmit}>
+          <label for="Date">Date :<br/>
+          <input type = "date" id="Date" name="Date" />
+          </label>
+          <br></br>
+          
+          <button type="button" onClick={this.handleSubmit}>Submit</button>
+          </form>
+          </div>
+          <div id ="output">
+          <p>{data}</p>
+          
         
-       
-        <br/>Date : <input type = "date"/> <br/>
-        <button type="Submit" class="btn btn-primary">Submit</button>
- 
-       
-
-    </form>
-    
+        </div>
+      
+    </div>
   );
-}
 
+}
+}
 export default App;
